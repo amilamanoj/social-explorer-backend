b5cd2f144c14b7138179c2c890d379592009cd29
Amila Silva <amilamanoj@r049239.osh.swh.mhn.de>
2016-06-28 19:43:06 +0200

    application status

--- application/applicationSchema.js
+++ application/applicationSchema.js
@@ -4,6 +4,11 @@ var mongoose = require('mongoose');
 // Define our application schema
 var Application = new mongoose.Schema({
     createdDate: Date,
+    status: {
+        type: String,
+        enum : ['PENDING','ACCEPTED','REJECTED'],
+        default : 'PENDING'
+    },
     host: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
