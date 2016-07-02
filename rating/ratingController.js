94ac819f75c960c265560d754ba05f3d51779265
Amila Silva <amilamanoj@78.198.eduroam.dynamic.rbg.tum.de>
2016-07-02 13:59:05 +0200

    populate users and projects in ratings

--- rating/ratingController.js
+++ rating/ratingController.js
@@ -3,7 +3,7 @@ var User = require('../user/userSchema');
 var Project = require('../project/projectSchema');
 
 
-exports.postRating = function(req, res) {
+exports.postRating = function (req, res) {
 
     var rating = new Rating(req.body);
 
@@ -13,7 +13,7 @@ exports.postRating = function(req, res) {
         res.sendStatus(401);
     }
 
-    rating.save(function(err, m) {
+    rating.save(function (err, m) {
         if (err) {
             res.status(500).send(err);
             return;
@@ -24,58 +24,48 @@ exports.postRating = function(req, res) {
 };
 
 // Create endpoint /api/rating for GET
-exports.getRatings = function(req, res) {
+exports.getRatings = function (req, res) {
     console.log("getting rating");
-    Rating.find(req.query, function(err, rating) {
+    Rating.find(req.query).populate('createdUser').populate('project').exec(function (err, rating) {
         if (err) {
             res.status(500).send(err);
             return;
         }
         var varIndex;
-        var sumRate=0;
+        var sumRate = 0;
 
-         for(varIndex = 0; varIndex < rating.length; ++varIndex){
-             var currRating = rating[varIndex];
-             User.findById(currRating.ratedUser, function(err, user) {
-                 if (err) {
-                     console.log('Error finding user')
-                 }
-                 console.log(user.username);
-                 console.log(rating[varIndex]);
-                 rating[varIndex].cUser = user.username;
-
-             });
-             sumRate+=rating[varIndex].rate;
-         }
-        // rating.average=(sumRate/rating.length) where I have to store this value???
-        var test=sumRate/rating.length
-        console.log(("hahdajdsfs"+sumRate/rating.length));
-            if(rating.length==0){
-                res.json(rating);
-            }
+        for (varIndex = 0; varIndex < rating.length; ++varIndex) {
+            var currRating = rating[varIndex];
+            sumRate += rating[varIndex].rate;
+        }
+        var average = sumRate / rating.length
+        if (rating.length == 0) {
+            res.json(rating);
+        }
         else {
-                rating[0].rateAvg = test;
-                res.json(rating);
-            }
+            rating[0].rateAvg = average;
+            res.json(rating);
+        }
 
     });
 };
 
 
 // Create endpoint /api/rating/:rating_id for GET
-exports.getRating = function(req, res) {
+exports.getRating = function (req, res) {
     // Use the Beer model to find a specific beer
-    Rating.findById(req.params.rating_id, function(err, rating) {
+    Rating.findById(req.params.rating_id, function (err, rating) {
         if (err) {
             res.status(500).send(err)
             return;
-        };
+        }
+        ;
         res.json(rating);
     });
 };
 
 // Create endpoint /api/rating/:rating_id for PUT
-exports.putRating = function(req, res) {
+exports.putRating = function (req, res) {
     // Use the Beer model to find a specific beer
     Rating.findByIdAndUpdate(
         req.params.rating_id,
@@ -96,9 +86,9 @@ exports.putRating = function(req, res) {
 };
 
 // Create endpoint /api/rating/:rating_id for DELETE
-exports.deleteRating = function(req, res) {
+exports.deleteRating = function (req, res) {
     // Use the Beer model to find a specific beer and remove it
-    Rating.findById(req.params.rating_id, function(err, m) {
+    Rating.findById(req.params.rating_id, function (err, m) {
         if (err) {
             res.status(500).send(err);
             return;
